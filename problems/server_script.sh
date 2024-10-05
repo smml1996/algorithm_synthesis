579dd8b73c117a6f7f286785729a0001d18bfef0
Stefanie Muroya Lei <stefanie.muroya@ucsp.edu.pe>
2024-10-05 08:40:51 +0200

    server script done

---
+++ problems/server_script.sh
@@ -0,0 +1,10 @@
+sbatch sscript.sh 5
+sbatch sscript.sh 20
+sbatch sscript.sh 7
+sbatch sscript.sh 27
+sbatch sscript.sh 65
+sbatch sscript.sh 28
+sbatch sscript.sh 16
+sbatch sscript.sh 15
+sbatch sscript.sh 53
+sbatch sscript.sh 127
