e3c5a540cb31e4e639ed6586e27dabeb85e8842f
smml1996 <stefanie.muroya@ucsp.edu.pe>
2025-01-06 15:56:27 -0500

    exp5

--- ig_problems/state_discrimination.py
+++ ig_problems/state_discrimination.py
@@ -1,3 +1,4 @@
+from cmath import isclose
 from enum import Enum
 from typing import Dict
 import sys, os
@@ -9,17 +10,15 @@ from ibm_noise_models import Instruction
 import qmemory
 from qpu_utils import Op
 from qstates import QuantumState
-from utils import are_matrices_equal, find_enum_object
+from utils import Precision, are_matrices_equal, find_enum_object
 from cmemory import ClassicalState, cread
 from experiments_utils import gen_ig_algorithm, rho_qubit0, rho_qubit1
 from pomdp import POMDPAction, POMDPVertex
 
 
 class StateDiscrExperimentID(Enum):
-    exp1 = "exp1" # 2 qubits, all possible clifford gates
-    exp2 = "exp2" # 3 qubits, all possible clifford gates
-    exp3 = "exp3" # 3 qubits, MEAS, H and CX
     exp4 = "exp4" # 1 qubit, extra classical space to store outcomes
+    exp5 = "exp5" # 2 qubits, H CX MEAS, non destructive measurement
     
     @property
     def exp_name(self):
@@ -30,7 +29,7 @@ class IGStateDiscrInstance:
         self.experiment_id = experiment_id
         self.embedding = embedding
         self.initial_states = None
-        if self.experiment_id in [StateDiscrExperimentID.exp1]:
+        if self.experiment_id in [StateDiscrExperimentID.exp5]:
             assert len(embedding) == 3
             self.hidden_qubit = 2
             self.remove_qubits = [0,1]
@@ -39,9 +38,7 @@ class IGStateDiscrInstance:
             self.hidden_qubit = 1
             self.remove_qubits = [0]
         else:
-            assert self.experiment_id in [StateDiscrExperimentID.exp2, StateDiscrExperimentID.exp3]
-            self.remove_qubits = [0,1,2]
-            self.hidden_qubit = 3
+            raise Exception(f"invalid experiment_id {experiment_id}")
         self.get_initial_states()
         
     def get_initial_states(self):
@@ -73,28 +70,19 @@ class IGStateDiscrInstance:
 def get_experiments_actions(embedding, experiment_id):
     assert isinstance(experiment_id, StateDiscrExperimentID)
     
-    if experiment_id in [StateDiscrExperimentID.exp1, StateDiscrExperimentID.exp2, StateDiscrExperimentID.exp3, StateDiscrExperimentID.exp4]:
-        if experiment_id in [StateDiscrExperimentID.exp1]:
-            num_qubits = 2
-            hidden_qubit = 2
-            assert len(embedding.keys()) == 3
-        elif experiment_id ==  StateDiscrExperimentID.exp4:
-            num_qubits = 1
-            hidden_qubit = 1
-            assert len(embedding.keys()) == 2
-        else:
-            num_qubits = 3
-            hidden_qubit = 3
-            assert len(embedding.keys()) == 4
+    if experiment_id in [StateDiscrExperimentID.exp4]:
+    
+        num_qubits = 1
+        hidden_qubit = 1
+        assert len(embedding.keys()) == 2
         
         actions = []
         
         # all clifford gates
         for i in range(num_qubits):
-            if experiment_id in [StateDiscrExperimentID.exp3, StateDiscrExperimentID.exp4]:
-                ops = [Op.H, Op.MEAS]
-            else:
-                ops = [Op.H, Op.S, Op.MEAS]
+            assert experiment_id in [StateDiscrExperimentID.exp5, StateDiscrExperimentID.exp4]
+            ops = [Op.H, Op.MEAS]
+            
             for op in ops:
                 actions.append(POMDPAction(f"{op.name}{i}", [Instruction(i, op)]))
             for j in range(i+1, num_qubits):
@@ -105,30 +93,57 @@ def get_experiments_actions(embedding, experiment_id):
                                       ])
         DETERMINE0 = POMDPAction("IS0", [Instruction(hidden_qubit, Op.WRITE1)])
         DETERMINEPlus = POMDPAction("ISPlus", [Instruction(hidden_qubit+1, Op.WRITE1)])
-        CAncilla0 = POMDPAction("CAncilla0", [Instruction(hidden_qubit+2, Op.WRITE0)])
-        CAncilla1 = POMDPAction("CAncilla1", [Instruction(hidden_qubit+2, Op.WRITE1)])
         actions.append(RESET)
         actions.append(DETERMINE0)
         actions.append(DETERMINEPlus)
-        if experiment_id == StateDiscrExperimentID.exp4:
-            pass
-            # actions.append(CAncilla0)
-            # actions.append(CAncilla1)
         print("******")
         for action in actions:
             print(action.name)
         return actions
+    elif experiment_id in [StateDiscrExperimentID.exp5]:
+        num_qubits = 2
+        hidden_qubit = 2
+        assert len(embedding.keys()) == 3
+        
+        actions = []
+        
+        # all clifford gates
+        for i in range(num_qubits):
+            ops = [Op.H, Op.S]
+            for op in ops:
+                actions.append(POMDPAction(f"{op.name}{i}", [Instruction(i, op)]))
+            for j in range(num_qubits):
+                if j !=i:
+                    actions.append(POMDPAction(f"CX{i}{j}", [Instruction(j, Op.CNOT, i)]))
+        
+        actions.append(POMDPAction("MEAS1", [Instruction(embedding[1], Op.MEAS)]))
+        DETERMINE0 = POMDPAction("IS0", [Instruction(hidden_qubit, Op.WRITE1)])
+        DETERMINEPlus = POMDPAction("ISPlus", [Instruction(hidden_qubit+1, Op.WRITE1)])
+        CAncilla0 = POMDPAction("CAncilla0", [Instruction(hidden_qubit+2, Op.WRITE0)])
+        CAncilla1 = POMDPAction("CAncilla1", [Instruction(hidden_qubit+2, Op.WRITE1)])
+        CAncilla20 = POMDPAction("CAncilla20", [Instruction(hidden_qubit+3, Op.WRITE0)])
+        CAncilla21 = POMDPAction("CAncilla21", [Instruction(hidden_qubit+3, Op.WRITE1)])
+        actions.append(DETERMINE0)
+        actions.append(DETERMINEPlus)
+        actions.append(CAncilla0)
+        actions.append(CAncilla1)
+        actions.append(CAncilla20)
+        actions.append(CAncilla21)
+        print("******")
+        for action in actions:
+            print(action.name)
+        return actions
+
     else:
         raise Exception(f"experiment actions not defined for experiment {experiment_id}")
     
 def get_experiment_id_qubits(experiment_id) -> int:
-    if experiment_id in [StateDiscrExperimentID.exp1]:
+    if experiment_id in [StateDiscrExperimentID.exp5]:
         return 3
     elif experiment_id == StateDiscrExperimentID.exp4:
         return 2
     else:
-        assert experiment_id in [StateDiscrExperimentID.exp2, StateDiscrExperimentID.exp3]
-        return 4
+        raise Exception(f"Invalid experiment id {experiment_id}")
 
 def get_experiment_id_horizon(experiment_id) -> int:
     return -1
@@ -136,7 +151,7 @@ def get_experiment_id_horizon(experiment_id) -> int:
 def guard(vertex: POMDPVertex, embedding: Dict[int, int], action: POMDPAction) -> bool:
     cs = vertex.classical_state
     
-    hidden_qubit = 2
+    hidden_qubit = len(embedding.keys()) - 1
     
     if cread(cs, hidden_qubit) == 1 and cread(cs, hidden_qubit+1) == 1:
         return action.name == "RESET"
@@ -147,6 +162,26 @@ def guard(vertex: POMDPVertex, embedding: Dict[int, int], action: POMDPAction) -
     
     return True
 
+def non_destructive_guard(vertex: POMDPVertex, embedding: Dict[int, int], action: POMDPAction) -> bool:
+    if not guard(vertex, embedding, action):
+        return False
+    
+    if action.name == "MEAS1":
+        assert len(embedding) == 3
+        remove_qubits = [0,2]
+        current_rho = vertex.quantum_state.multi_partial_trace(remove_indices=remove_qubits)
+        assert len(current_rho) == len(current_rho[0])
+        assert len(current_rho) == 2
+        return isclose(current_rho[0][0] + current_rho[1][1], 1, rel_tol=Precision.rel_tol)
+        
+    return True
+
+def get_guard(experiment_id):
+    if experiment_id == StateDiscrExperimentID.exp4:
+        return guard
+    assert experiment_id == StateDiscrExperimentID.exp5
+    return non_destructive_guard
+
 if __name__ == "__main__":
     arg_exp = sys.argv[1]
     experiment_id = find_enum_object(arg_exp, StateDiscrExperimentID)
@@ -156,7 +191,7 @@ if __name__ == "__main__":
                     get_experiment_id_qubits(experiment_id),
                     get_experiments_actions,
                     IGStateDiscrInstance,
-                    guard,
+                    get_guard(experiment_id),
                     get_experiment_id_horizon(experiment_id))
 
     
